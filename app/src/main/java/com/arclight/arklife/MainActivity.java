bcb26b8c678f4bc6a93b4af538c3bbe6e8410a3e
hawwen <nghawwen1997@gmail.com>
2019-09-24 00:12:17 +0800

    Implement main splash page

--- app/src/main/java/com/arclight/arklife/MainActivity.java
+++ app/src/main/java/com/arclight/arklife/MainActivity.java
@@ -1,24 +1,55 @@
 package com.arclight.arklife;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.util.Log;
-
-import com.amazonaws.mobile.client.AWSMobileClient;
-import com.amazonaws.mobile.client.AWSStartupHandler;
-import com.amazonaws.mobile.client.AWSStartupResult;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        AWSMobileClient.getInstance().initialize(this, new AWSStartupHandler() {
+        Button button1 = (Button) findViewById(R.id.button1);
+        imageView = (ImageView) findViewById(R.id.imageViewid);
+
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onComplete(AWSStartupResult awsStartupResult) {
-                Log.d("MainActivity", "AWSMobileClient is instantiated and you are connected to AWS!");
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
+
             }
-        }).execute();
+        });
+        Button button2 = (Button)findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), com.arclight.arklife.button2.class);
+                startIntent.putExtra("com.example.androidgridlayout.SOME", "HELL");
+                startActivity(startIntent);
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        imageView.setImageBitmap(bitmap);
+
+    }
+
+
+
 }
